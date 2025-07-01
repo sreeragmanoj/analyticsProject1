80a6a46b84278402553a8f21c02321419eee4526
sreerag-manoj <sreerag.mk@wecodecommunity.com>
2025-07-01 15:35:13 +0530

    Refactor API event handling and schemas for improved structure and functionality

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,15 +1,34 @@
 from fastapi import APIRouter
-from .schemas import EventSchema
+from .schemas import EventSchema, EventListSchema, EventCreateSchema, EventUpdateSchema
 router = APIRouter()
 
+
+# get method for listing the events
 @router.get('/')
-def read_events():
+def read_events() -> EventSchema:
     return {
-        "items": [1, 2, 3],
+        "results": [{'event_id': 1}, {'event_id': 2}, {'event_id': 3}]
     }
 
+# post method for creating an event
+@router.post('/')
+def create_event(payload: EventCreateSchema) -> EventSchema:
+    data = payload.model_dump()
+    return { "event_id": 1, **data}
+#
+# get method for reading a specific event by its ID
 @router.get("/{event_id}")
 def read_event(event_id: int) -> EventSchema:
     return {
         "event_id": event_id,
     }
+
+# Put method for updating an event
+@router.put("/{event_id}")
+def read_event(event_id: int, payload: EventUpdateSchema) -> EventSchema:
+    data = payload.model_dump()
+    return {
+        "event_id": event_id,
+        **data
+    }
+
--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,4 +1,17 @@
 from pydantic import BaseModel
+from typing import List, Optional
 
 class EventSchema(BaseModel):
     event_id: int
+    page: Optional[str] = ""
+    description: Optional[str] = ""
+
+class EventListSchema(BaseModel):
+    results: List[EventSchema]
+
+class EventCreateSchema(BaseModel):
+    page: str
+    description: Optional[str] = ""
+
+class EventUpdateSchema(BaseModel):
+    description: str
